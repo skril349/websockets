68207d5f5b6eda51529eec0972d80568b7961a19
skril349 <tvives.cabaleiro@gmail.com>
2024-02-16 19:38:05 +0100

    connect and disconnect useEffects

--- 03-bandnames/bandnames-client/src/App.js
+++ 03-bandnames/bandnames-client/src/App.js
@@ -1,15 +1,48 @@
+import { useEffect, useState } from "react";
 import BandAdd from "./components/BandAdd";
 import BandList from "./components/BandList";
+import io from 'socket.io-client'
+
+const connectSocketServer = () =>{
+  const socket = io.connect('http://localhost:8080')
+  return socket;
+}
 
 function App() {
+  const [socket] = useState(connectSocketServer(),{
+    transports:['websocket']
+  });
+  const [online,setOnline] = useState(false);
+
+  useEffect(()=>{
+    console.log(socket)
+    setOnline(socket.connected)
+  },[socket])
+
+  useEffect(()=>{
+    socket.on('connect', ()=>{
+      setOnline(true)
+    })
+  },[socket])
+  useEffect(()=>{
+    socket.on('disconnect', ()=>{
+      setOnline(false)
+    })
+  },[socket])
+
   return (
     <div className="container"> 
 
     <div className="alert">
       <p>
         Service Status:
-        <span className="text-success">Online</span>
-        <span className="text-danger">Offline</span>
+        {
+          online
+            ?<span className="text-success">Online</span>
+            :<span className="text-danger">Offline</span>
+
+
+        }
 
       </p>
     </div>

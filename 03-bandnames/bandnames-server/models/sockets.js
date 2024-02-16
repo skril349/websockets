f50d6ddc92566efdfb5ec0bdb9225f1d75bd513d
skril349 <tvives.cabaleiro@gmail.com>
2024-02-16 19:01:59 +0100

    creating socket communication

---
+++ 03-bandnames/bandnames-server/models/sockets.js
@@ -0,0 +1,30 @@
+const BandList = require('./band-list');
+
+
+class Sockets {
+
+    constructor( io ) {
+
+        this.io = io;
+
+        this.bandList = new BandList();
+
+        this.socketEvents();
+    }
+
+    socketEvents() {
+        // On connection
+        this.io.on('connection', ( socket ) => {
+
+            console.log("cliente conectado")
+            // Emitir al cliente conectado todas las bandas actuales
+            socket.emit('current-bands', this.bandList.getBands())
+
+        });
+    }
+
+
+}
+
+
+module.exports = Sockets;

413f430a339a776200339ac7ed8ee308ee2c1624
skril349 <tvives.cabaleiro@gmail.com>
2024-02-16 22:27:31 +0100

    adding real time graphic chart

--- 03-bandnames/bandnames-client/src/components/BandChart.js
+++ 03-bandnames/bandnames-client/src/components/BandChart.js
@@ -1,43 +1,56 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import Chart from 'chart.js/auto';
- 
+import { SocketContext } from '../context/SocketContext';
+
 const BandChart = () => {
-  const chartRef = useRef(null);
- 
-  useEffect(() => {
-    const ctx = chartRef.current;
-    if (ctx) {
-      // Destruir el gráfico existente si existe
-      if (ctx.chart) {
-        ctx.chart.destroy();
-      }
- 
-      // Crear un nuevo gráfico
-      ctx.chart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-          datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            borderWidth: 1
-          }]
-        },
-        options: {
-            indexAxis: 'y',
-          scales: {
-            y: {
-              beginAtZero: true
+    const chartRef = useRef(null);
+    const { socket } = useContext(SocketContext)
+    const [bands, setBands] = useState([]);
+
+
+    useEffect(() => {
+        socket.on('current-bands', (bands) => {
+           
+            console.log("bands =", bands)
+            crearGrafica(bands)
+        })
+        return () => socket.off('current-bands')
+    }, [socket])
+
+    const crearGrafica = (bands = [], votes = [])=>{
+        const ctx = chartRef.current;
+        if (ctx) {
+            // Destruir el gráfico existente si existe
+            if (ctx.chart) {
+                ctx.chart.destroy();
             }
-          }
+
+            // Crear un nuevo gráfico
+            ctx.chart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: bands.map((band)=> band.name),
+                    datasets: [{
+                        label: '# of Votes',
+                        data:bands.map((band)=> band.votes),
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    indexAxis: 'y',
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
         }
-      });
     }
-  }, []);
- 
-  return (
-    <canvas ref={chartRef}></canvas>
-  );
+
+    return (
+        <canvas ref={chartRef}></canvas>
+    );
 };
 
 export default BandChart

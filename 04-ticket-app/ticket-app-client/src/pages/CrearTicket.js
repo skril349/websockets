98372c19f825727f48782fa25ecc5a4b73fc3cea
skril349 <tvives.cabaleiro@gmail.com>
2024-02-19 19:01:28 +0100

    addinc createTicket paGE

--- 04-ticket-app/ticket-app-client/src/pages/CrearTicket.js
+++ 04-ticket-app/ticket-app-client/src/pages/CrearTicket.js
@@ -1,10 +1,32 @@
+import { DownloadOutlined } from '@ant-design/icons';
+import { Button, Col, Row, Typography } from 'antd'
 import React from 'react'
 
+const { Title, Text } = Typography;
+
 const CrearTicket = () => {
+
+  const nuevoTicket = () =>{
+    console.log("nuevo ticket")
+  }
   return (
-    <div>
-      Crear Ticket
-    </div>
+    <>
+      <Row>
+        <Col span={14} offset={6} align="center">
+          <Title level={3}> Presione el boton para un nuevo ticket :</Title>
+          <Button type='primary' success shape='round' icon={<DownloadOutlined/>} size='large' onClick={nuevoTicket}> 
+            nuevo ticket
+          </Button>
+        </Col>
+      </Row>
+      <Row style={{marginTop:100}}>
+        <Col span={14} offset={6} align="center">
+          <Text level={2}>Su numero </Text>
+          <br/>
+          <Text type='success' style={{fontSize:55}}>55</Text>         
+        </Col>
+      </Row>
+    </>
   )
 }
 

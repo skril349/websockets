3566f524bf40953487deaf210f0cdf087e160e54
skril349 <tvives.cabaleiro@gmail.com>
2024-02-19 18:52:26 +0100

    Creating escritorio page

--- 04-ticket-app/ticket-app-client/src/pages/Escritorio.js
+++ 04-ticket-app/ticket-app-client/src/pages/Escritorio.js
@@ -1,10 +1,47 @@
+import { ArrowLeftOutlined, ArrowRightOutlined, CloseCircleOutlined } from '@ant-design/icons';
+import { Button, Col, Divider, Row, Typography } from 'antd'
 import React from 'react'
 
+const { Title, Text } = Typography;
+
 const Escritorio = () => {
+
+  const salir = () => {
+    console.log("salir")
+  }
+
+  const siguienteTicket = () => {
+    console.log("siguienteTicket")
+  }
   return (
-    <div>
-      Escritorio page
-    </div>
+    <>
+      <Row>
+        <Col span={20}>
+          <Title level={2}>Fernando</Title>
+          <Text>Usted trabaja en el escritorio: </Text>
+          <Text type='success'>5</Text>
+        </Col>
+        <Col span={4} align="right">
+          <Button shape='round' type='primary' danger onClick={salir}>
+            <CloseCircleOutlined />
+          </Button>
+        </Col>
+      </Row>
+      <Divider />
+
+      <Row>
+        <Col>
+          <Text>Esta atendiendo el ticket numero : </Text>
+          <Text style={{ fontSize: 30 }} type='danger'>55</Text>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col offset={18} span={6} align="right">
+          <Button shape='round' type='primary' onClick={siguienteTicket}> <ArrowRightOutlined/>Siguiente</Button>
+        </Col>
+      </Row>
+    </>
   )
 }
 

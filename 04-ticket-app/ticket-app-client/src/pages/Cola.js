d98b886cb8a28e33288c76c8c3960454acc08e23
skril349 <tvives.cabaleiro@gmail.com>
2024-02-19 19:13:24 +0100

    creating cola page

--- 04-ticket-app/ticket-app-client/src/pages/Cola.js
+++ 04-ticket-app/ticket-app-client/src/pages/Cola.js
@@ -1,10 +1,102 @@
+import { Card, Col, Divider, List, Row,Tag,Typography } from 'antd';
 import React from 'react'
+
+const { Title, Text } = Typography;
+
+
+const data = [
+  {
+      ticketNo: 33,
+      escritorio: 3,
+      agente: 'Fernando Herrera'
+  },
+  {
+      ticketNo: 34,
+      escritorio: 4,
+      agente: 'Melissa Flores'
+  },
+  {
+      ticketNo: 35,
+      escritorio: 5,
+      agente: 'Carlos Castro'
+  },
+  {
+      ticketNo: 36,
+      escritorio: 3,
+      agente: 'Fernando Herrera'
+  },
+  {
+      ticketNo: 37,
+      escritorio: 3,
+      agente: 'Fernando Herrera'
+  },
+  {
+      ticketNo: 38,
+      escritorio: 2,
+      agente: 'Melissa Flores'
+  },
+  {
+      ticketNo: 39,
+      escritorio: 5,
+      agente: 'Carlos Castro'
+  },
+];
 
 const Cola = () => {
   return (
-    <div>
-      Cola Page
-    </div>
+    <>
+      <Title level={1}>
+        Atendiendo al cliente
+      </Title>
+      <Row>
+        <Col span={12}>
+          <List 
+          dataSource={data.slice(0,3)}
+          renderItem={ item => (
+            <List.Item>
+              <Card
+              style={{width:300, marginTop:16}}
+              actions={[
+                <Tag color='volcano'>{item.agente}</Tag>,
+                <Tag color='magenta'>Escritorio {item.escritorio}</Tag>
+
+              ]}
+              >
+                <Title>No. {item.ticketNo}</Title>
+              </Card>
+            </List.Item>
+          ) }
+
+          />
+
+        </Col>
+        <Col span={12}>
+              <Divider>
+                Historial:
+              </Divider>
+              <List
+              dataSource={data.slice(3)}
+              renderItem={item =>(
+                <List.Item.Meta
+                title = {`ticket No. ${item.ticketNo}`}
+                description={
+                  <>
+                    <Text>En el escritorio</Text>
+                    <Tag color='magenta'>{item.ticketNo}</Tag>
+                    <Text>Agente: </Text>
+                    <Tag color='volcano'>{item.agente}</Tag>
+                  </>
+                }
+                >
+
+                </List.Item.Meta>
+              )}
+              >
+
+              </List>
+        </Col>
+      </Row>
+    </>
   )
 }
 

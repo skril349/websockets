7ecedd50f5be577816f290e1b31c2285b9897f3e
skril349 <tvives.cabaleiro@gmail.com>
2024-02-19 18:06:59 +0100

    adding routers

--- 04-ticket-app/ticket-app-client/src/pages/RouterPage.js
+++ 04-ticket-app/ticket-app-client/src/pages/RouterPage.js
@@ -1,75 +1,82 @@
 import React, { useState } from 'react';
-import {
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from '@ant-design/icons';
 import { Layout, Menu, Button, theme } from 'antd';
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from "react-router-dom";
+import Ingresar from './Ingresar';
+import Cola from './Cola';
+import CrearTicket from './CrearTicket';
+import Escritorio from './Escritorio';
+import { MenuFoldOutlined, MenuUnfoldOutlined, UserOutlined, VideoCameraOutlined, UploadOutlined } from '@ant-design/icons';
 
 const { Header, Sider, Content } = Layout;
 
 const RouterPage = () => {
-
     const [collapsed, setCollapsed] = useState(false);
     const {
-      token: { colorBgContainer, borderRadiusLG },
+        token: { colorBgContainer, borderRadiusLG },
     } = theme.useToken();
-  return (
-    <Layout style={{height:'100vh'}}>
-    <Sider trigger={null} collapsible collapsed={collapsed}>
-      <div className="demo-logo-vertical" />
-      <Menu
-        theme="dark"
-        mode="inline"
-        defaultSelectedKeys={['1']}
-        items={[
-          {
-            key: '1',
-            icon: <UserOutlined />,
-            label: 'nav 1',
-          },
-          {
-            key: '2',
-            icon: <VideoCameraOutlined />,
-            label: 'nav 2',
-          },
-          {
-            key: '3',
-            icon: <UploadOutlined />,
-            label: 'nav 3',
-          },
-        ]}
-      />
-    </Sider>
-    <Layout>
-      <Header style={{ padding: 0, background: colorBgContainer }}>
-        <Button
-          type="text"
-          icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-          onClick={() => setCollapsed(!collapsed)}
-          style={{
-            fontSize: '16px',
-            width: 64,
-            height: 64,
-          }}
-        />
-      </Header>
-      <Content
-        style={{
-          margin: '24px 16px',
-          padding: 24,
-          minHeight: 280,
-          background: colorBgContainer,
-          borderRadius: borderRadiusLG,
-        }}
-      >
-        Content
-      </Content>
-    </Layout>
-  </Layout>
-  )
+
+    return (
+       <Router>
+         <Layout style={{ height: '100vh' }}>
+            <Sider trigger={null} collapsible collapsed={collapsed}>
+                <div className="demo-logo-vertical" />
+                <Menu
+                    theme="dark"
+                    mode="inline"
+                    defaultSelectedKeys={['1']}
+                    items={[
+                        {
+                            key: '1',
+                            icon: <UserOutlined />,
+                            label: <Link to="/ingresar">Ingresar</Link>,
+                        },
+                        {
+                            key: '2',
+                            icon: <VideoCameraOutlined />,
+                            label: <Link to="/cola">Cola</Link>,
+                        },
+                        {
+                            key: '3',
+                            icon: <UploadOutlined />,
+                            label: <Link to="/crear-tickets">Crear Tickets</Link>,
+                        },
+                    ]}
+                />
+            </Sider>
+            <Layout>
+                <Header style={{ padding: 0, background: colorBgContainer }}>
+                    <Button
+                        type="text"
+                        icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                        onClick={() => setCollapsed(!collapsed)}
+                        style={{
+                            fontSize: '16px',
+                            width: 64,
+                            height: 64,
+                        }}
+                    />
+                </Header>
+                <Content
+                    style={{
+                        margin: '24px 16px',
+                        padding: 24,
+                        minHeight: 280,
+                        background: colorBgContainer,
+                        borderRadius: borderRadiusLG,
+                    }}
+                >
+                    <Routes>
+                        <Route path='/ingresar' element={<Ingresar />} />
+                        <Route path='/cola' element={<Cola />} />
+                        <Route path='/crear-tickets' element={<CrearTicket />} />
+                        <Route path='/escritorio' element={<Escritorio />} />
+                        <Route path='*' element={<Navigate to="/ingresar" replace />} />
+                    </Routes>
+                </Content>
+            </Layout>
+        </Layout>
+       </Router>
+    );
 }
 
-export default RouterPage
+export default RouterPage;

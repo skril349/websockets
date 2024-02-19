81e6ef5ec335a128511fd922236819f26455df38
skril349 <tvives.cabaleiro@gmail.com>
2024-02-19 18:41:20 +0100

    on finish navigate to desktop

--- 04-ticket-app/ticket-app-client/src/pages/Ingresar.js
+++ 04-ticket-app/ticket-app-client/src/pages/Ingresar.js
@@ -1,86 +1,63 @@
 import React from 'react';
-import { Button, Form, Input, InputNumber } from 'antd';
-import {SaveOutlined} from '@ant-design/icons'
-const onFinish = (values) => {
-  console.log('Success:', values);
-};
-const onFinishFailed = (errorInfo) => {
-  console.log('Failed:', errorInfo);
-};
+import { Button, Divider, Form, Input, InputNumber, Typography } from 'antd';
+import { SaveOutlined } from '@ant-design/icons';
+import { useNavigate } from 'react-router-dom'; // Updated import
 
+
+const { Title, Text } = Typography;
 
 const Ingresar = () => {
+  const navigate = useNavigate(); // Updated to use useNavigate
 
+  const onFinish = (values) => {
+    console.log('Success:', values);
+    navigate('/escritorio'); // Add the path you want to navigate to
   
+  };
   
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
   return (
-    <Form
-    name="basic"
-    labelCol={{
-      span: 4,
-    }}
-    wrapperCol={{
-      span: 14,
-    }}
-    style={{
-      maxWidth: 600,
-    }}
-    initialValues={{
-      remember: true,
-    }}
-    onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
-    autoComplete="off"
-  >
-    <Form.Item
-      label="Nombre de la gente"
-      name="Nombre de la gente"
-      rules={[
-        {
-          required: true,
-          message: 'Porfavor ingrese su nombre!',
-        },
-      ]}
-    >
-      <Input />
-    </Form.Item>
+    <>
+      <Title level={2}>Ingresar</Title>
+      <Text>Ingrese su nombre y numero de escritorio</Text>
+      <Divider />
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Nombre de la gente"
+          name="Nombre de la gente"
+          rules={[{ required: true, message: 'Porfavor ingrese su nombre!' }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="escritorio"
+          name="escritorio"
+          rules={[{ required: true, message: 'Introduce su escritorio!' }]}
+        >
+          <InputNumber min={1} max={99} />
+        </Form.Item>
+        <Form.Item
+          wrapperCol={{ offset: 8, span: 16 }}
+        >
+          <Button type="primary" htmlType="submit" shape='round'>
+            <SaveOutlined />
+            Ingresar
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
+};
 
-    <Form.Item
-      label="escritorio"
-      name="escritorio"
-      rules={[
-        {
-          required: true,
-          message: 'Introduce su escritorio!',
-        },
-      ]}
-    >
-      <InputNumber min={1} max={99} />
-    </Form.Item>
-
-    <Form.Item
-      name="remember"
-      valuePropName="checked"
-      wrapperCol={{
-        offset: 8,
-        span: 16,
-      }}
-    >
-    </Form.Item>
-
-    <Form.Item
-      wrapperCol={{
-        offset: 8,
-        span: 16,
-      }}
-    >
-      <Button type="primary" htmlType="submit" shape='round'>
-      <SaveOutlined />
-        Ingresar
-      </Button>
-    </Form.Item>
-  </Form>
-  )
-}
-
-export default Ingresar
+export default Ingresar;

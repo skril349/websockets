a9a16e4d8efeb07db3cf1b0f5841aaaf43219a0e
skril349 <tvives.cabaleiro@gmail.com>
2024-02-22 19:33:52 +0100

    transforming screens

--- 06-chat/client-chat/src/router/AuthRouter.js
+++ 06-chat/client-chat/src/router/AuthRouter.js
@@ -4,11 +4,13 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { LoginPage } from "../pages/LoginPage";
 import { RegisterPage } from "../pages/RegisterPage";
 
+import "../css/login-register.css"
+
 export const AuthRouter = () => {
     return (
         <div className="limiter">
-            <div className="container-login100">
-                <div className="wrap-login100 p-t-50 p-b-90">
+		<div className="container-login100">
+			<div className="wrap-login100 p-t-50 p-b-90">
                     <Routes>
                         <Route path="/login" element={<LoginPage />} />
                         <Route path="/register" element={<RegisterPage />} />

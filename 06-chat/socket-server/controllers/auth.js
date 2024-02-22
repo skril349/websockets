a1eb2f385093bf25debe8a6e23db5486b2a0f7e2
skril349 <tvives.cabaleiro@gmail.com>
2024-02-22 13:23:03 +0100

    pushing data on DB

--- 06-chat/socket-server/controllers/auth.js
+++ 06-chat/socket-server/controllers/auth.js
@@ -2,7 +2,7 @@ const { response } = require('express');
 const Usuario = require('../models/usuario');
 
 
-const crearUsuario = async(req,res = response)=>{
+const crearUsuario = async (req, res = response) => {
 
     try {
 
@@ -10,37 +10,49 @@ const crearUsuario = async(req,res = response)=>{
 
         // verificar que el email no exista
 
-        const existeEmail = await Usuario.findOne({email})
+        const existeEmail = await Usuario.findOne({ email })
+        if (existeEmail) {
+            return res.status(400).json({
+                ok: false,
+                msg: "El email ya existe"
+            })
+        }
+
+        // TODO encriptar contraseña
+
+        // Guardar en BD
+
+        const usuario = new Usuario(req.body);
+        await usuario.save();
 
 
         res.json({
-            email,
-            password
+            usuario
         })
-        
+
     } catch (error) {
         console.log(error)
         res.status(500).json({
-            ok:false,
-            msg:"Hable con el administrador"
+            ok: false,
+            msg: "Hable con el administrador"
         })
     }
 }
 
-const RenewToken = async(req,res = response)=>{
+const RenewToken = async (req, res = response) => {
     res.json({
-        ok:true,
-        msg:"renew"
+        ok: true,
+        msg: "renew"
     })
 }
 
-const Login = async(req,res = response)=>{
+const Login = async (req, res = response) => {
 
-  
-    const {email,password} = req.body;
+
+    const { email, password } = req.body;
     res.json({
-        ok:true,
-        msg:'login',
+        ok: true,
+        msg: 'login',
         email,
         password
     })

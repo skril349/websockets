027129fab54f67acc308db367888c6fe03217810
skril349 <tvives.cabaleiro@gmail.com>
2024-02-22 16:35:55 +0100

    adding login

--- 06-chat/socket-server/controllers/auth.js
+++ 06-chat/socket-server/controllers/auth.js
@@ -59,12 +59,42 @@ const Login = async (req, res = response) => {
 
 
     const { email, password } = req.body;
-    res.json({
-        ok: true,
-        msg: 'login',
-        email,
-        password
+   try {
+
+    const usuarioDB = await Usuario.findOne({email})
+     if(!usuarioDB){
+        return res.status(404).json({
+            ok:false,
+            msg:"Email no encontrado"
+        })
+     }
+
+     //Validem password
+     const validPassword = bcrypt.compareSync( password, usuarioDB.password )
+     if(!validPassword){
+        return res.status(400).json({
+            ok:false,
+            msg:"Password incorrecta"
+        })
+     }
+
+     // Generar JWT
+
+     const token = await generarJWT(usuarioDB.id)
+     res.json({
+        ok:true,
+        usuario:usuarioDB,
+        token
+     })
+
+
+   } catch (error) {
+    console.log(error)
+    res.status(500).json({
+        ok: false,
+        msg: "Hable con el administrador"
     })
+   }
 }
 
 module.exports = {

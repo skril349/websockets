3ca9be761a336df6088d404f23c10b520c826c77
skril349 <tvives.cabaleiro@gmail.com>
2024-02-22 18:08:59 +0100

    creating mensajes

--- 06-chat/socket-server/models/server.js
+++ 06-chat/socket-server/models/server.js
@@ -34,6 +34,7 @@ class Server {
 
         // API ENDPOINTS
         this.app.use( '/api/login',require('../router/auth') )
+        this.app.use('/api/mensajes',require('../router/mensajes') )
     }
 
     // Esta configuración se puede tener aquí o como propieda de clase

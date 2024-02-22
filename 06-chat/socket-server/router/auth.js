c90d1f248073099a40081d182d705c7167ae215b
skril349 <tvives.cabaleiro@gmail.com>
2024-02-22 17:04:12 +0100

    middleware to renew token

--- 06-chat/socket-server/router/auth.js
+++ 06-chat/socket-server/router/auth.js
@@ -8,6 +8,7 @@ const {Router} = require('express');
 const { crearUsuario, RenewToken, Login } = require('../controllers/auth');
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
 const router = Router();
 
 //Crear nuevos usuarios
@@ -29,6 +30,6 @@ router.post('/',[
 
 // Revalidar token
 
-router.get('/renew',RenewToken)
+router.get('/renew',validarJWT,RenewToken)
 
 module.exports = router;

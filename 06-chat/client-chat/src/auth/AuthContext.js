14b5e6877f417401c04cd25e6d2df065442db938
skril349 <tvives.cabaleiro@gmail.com>
2024-02-26 20:20:21 +0100

    renew token

--- 06-chat/client-chat/src/auth/AuthContext.js
+++ 06-chat/client-chat/src/auth/AuthContext.js
@@ -1,80 +1,122 @@
-import React, { createContext, useCallback, useState} from "react"
-import { fetchSinToken } from "../helpers/fetch"
+import React, { createContext, useCallback, useState } from "react"
+import { fetchConToken, fetchSinToken } from "../helpers/fetch"
 
 export const AuthContext = createContext()
 
 
 const initialState = {
 
-    uid:null,
-    checking:true,
-    logged:false,
-    name:null,
-    email:null,
+    uid: null,
+    checking: true,
+    logged: false,
+    name: null,
+    email: null,
 }
 
 
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
 
     const [auth, setAuth] = useState(initialState)
-    const login = async(email,password) =>{
+    const login = async (email, password) => {
 
-        const resp =await fetchSinToken("login", {email,password},"POST");
+        const resp = await fetchSinToken("login", { email, password }, "POST");
         console.log(resp.ok)
-        const {usuario} = resp
-        if(resp.ok){
-            localStorage.setItem('token',resp.token)
+        const { usuario } = resp
+        if (resp.ok) {
+            localStorage.setItem('token', resp.token)
             setAuth({
 
-                uid:usuario.uid,
-                checking:false,
-                logged:true,
-                name:usuario.name,
-                email:usuario.email,
+                uid: usuario.uid,
+                checking: false,
+                logged: true,
+                name: usuario.name,
+                email: usuario.email,
             })
         }
         return resp.ok
     }
 
-    const register = async (nombre,email,password)=>{
-        const resp =await fetchSinToken("login/new", {nombre,email,password},"POST");
+    const register = async (nombre, email, password) => {
+        const resp = await fetchSinToken("login/new", { nombre, email, password }, "POST");
         console.log(resp)
-        const {usuario,token} = resp
-        if(usuario){
-            localStorage.setItem('token',token)
+        const { usuario, token } = resp
+        if (usuario) {
+            localStorage.setItem('token', token)
             setAuth({
 
-                uid:usuario.uid,
-                checking:false,
-                logged:true,
-                name:usuario.name,
-                email:usuario.email,
+                uid: usuario.uid,
+                checking: false,
+                logged: true,
+                name: usuario.name,
+                email: usuario.email,
             })
             return true
         }
         return resp.msg
     }
 
-    const verificarToken = useCallback( ()=>{
+    const verificarToken = useCallback(async() => {
 
-    },[] )
+        const token = localStorage.getItem('token')
 
-    const logOut = () =>{
+        if (!token) {
+             setAuth({
+
+                uid: null,
+                checking: false,
+                logged: false,
+                name: null,
+                email: null,
+            })
+            return false
+        }
+
+        const resp = await fetchConToken('login/renew')
+        const { usuario } = resp
+
+        if(resp.ok){
+            localStorage.setItem('token', resp.token)
+            setAuth({
+
+                uid: usuario.uid,
+                checking: false,
+                logged: true,
+                name: usuario.name,
+                email: usuario.email,
+            })
+            return true
+        }else{
+             setAuth({
+
+                uid: null,
+                checking: false,
+                logged: false,
+                name: null,
+                email:null,
+            })
+            return false
+
+        }
+        
+
+    }, [])
+
+    const logOut = () => {
 
     }
 
-  return (
-    <AuthContext.Provider value={{
-        auth,
-        login,
-        register,
-        verificarToken,
-        logOut,
+    return (
+        <AuthContext.Provider value={{
+            auth,
+            login,
+            register,
+            verificarToken,
+            logOut,
 
-    }}>
-        {children}
-    </AuthContext.Provider>
-  )
+        }}>
+            {children}
+        </AuthContext.Provider>
+    )
 }
 
 export default AuthProvider
--- 06-chat/client-chat/src/router/AppRouter.js
+++ 06-chat/client-chat/src/router/AppRouter.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import {
     BrowserRouter as Router,
     Routes,
@@ -8,8 +8,20 @@ import {
 
 import { AuthRouter } from './AuthRouter';
 import { ChatPage } from '../pages/ChatPage';
+import { AuthContext } from '../auth/AuthContext';
 
 export const AppRouter = () => {
+
+    const {auth, verificarToken} = useContext(AuthContext)
+
+    useEffect(()=>{
+        verificarToken();
+    },[verificarToken])
+
+    if(auth.checking){
+        return <h1>Espere por favor</h1>
+    }
+
     return (
         <Router>
             <div>

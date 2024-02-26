f498f8b3b351fe9f254eb2f180cdc8b307ea16af
skril349 <tvives.cabaleiro@gmail.com>
2024-02-26 19:19:44 +0100

    localstorage

--- 06-chat/client-chat/src/pages/LoginPage.js
+++ 06-chat/client-chat/src/pages/LoginPage.js
@@ -1,27 +1,75 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {Link} from "react-router-dom"
 
 export const LoginPage = () => {
+
+	const [form, setForm] = useState({
+		email:" test1@test.com",
+		password: "123456",
+		rememberme:true,
+	})
+
+	useEffect(()=>{
+		const remembermeEmail = localStorage.getItem("email")
+		if(remembermeEmail){
+			setForm({
+				...form,
+				rememberme:true,
+				email:remembermeEmail
+			})
+		}
+	},[])
+
+	const onChange=({target})=>{
+		const {name,value} = target
+		setForm({
+			...form,
+			[name]:value
+		})
+	}
+
+	const toggleCheck=()=>{
+		setForm({
+			...form,
+			rememberme:!form.rememberme
+		})
+	}
+
+	const onSubmit = (ev) =>{
+		ev.preventDefault()
+		if(form.rememberme){
+			localStorage.setItem("email",form.email)
+		}else{
+			localStorage.removeItem("email")
+		}
+		// TODO: Llamar el backend
+	}
+
   return (
-    <form className="login100-form validate-form flex-sb flex-w">
+    <form className="login100-form validate-form flex-sb flex-w"
+	onSubmit={onSubmit}>
 					<span className="login100-form-title mb-3">
 						Chat - Ingreso
 					</span>
 					
 					<div className="wrap-input100 validate-input mb-3">
-						<input className="input100" type="email" name="email" placeholder="Email" />
+						<input className="input100" type="email" name="email" placeholder="Email" 
+						value={form.email}
+						onChange={onChange}/>
 						<span className="focus-input100"></span>
 					</div>
 					
 					
 					<div className="wrap-input100 validate-input mb-3">
-						<input className="input100" type="password" name="password" placeholder="Password" />
+						<input className="input100" type="password" name="password" placeholder="Password" value={form.password} onChange={onChange}/>
 						<span className="focus-input100"></span>
 					</div>
 					
 					<div className="row mb-3">
-						<div className="col">
-							<input className="input-checkbox100" id="ckb1" type="checkbox" name="remember-me" />
+						<div className="col" 
+							onClick={()=>toggleCheck()}
+						>
+							<input className="input-checkbox100" id="ckb1" type="checkbox" name="rememberme" checked={form.rememberme} readOnly/>
 							<label className="label-checkbox100">
 								Recordarme
 							</label>

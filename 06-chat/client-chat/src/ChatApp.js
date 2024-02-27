7e4bbdf72165d568805bb581cbe728ef7adb2f60
skril349 <tvives.cabaleiro@gmail.com>
2024-02-27 18:58:22 +0100

    chatContext and chatreducer

--- 06-chat/client-chat/src/ChatApp.js
+++ 06-chat/client-chat/src/ChatApp.js
@@ -2,14 +2,17 @@ import React from 'react'
 import { AppRouter } from './router/AppRouter'
 import AuthProvider from './auth/AuthContext'
 import { SocketProvider } from './context/SocketContext'
+import ChatProvider from './context/chat/ChatContext'
 
 const ChatApp = () => {
   return (
-    <AuthProvider>
+    <ChatProvider>
+      <AuthProvider>
       <SocketProvider>
       <AppRouter/>
       </SocketProvider>
     </AuthProvider>
+    </ChatProvider>
   )
 }
 
---
+++ 06-chat/client-chat/src/context/chat/chatReducer.js
@@ -0,0 +1,11 @@
+
+
+
+export const chatReducer = (state,action) =>{
+    switch (action.type) {
+       
+    
+        default:
+            return state;
+    }
+}

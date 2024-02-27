9dcf538e9258b8f5b5b51e37ebc1d1c0189fdd67
skril349 <tvives.cabaleiro@gmail.com>
2024-02-27 20:01:49 +0100

    getting message

--- 06-chat/client-chat/src/components/SendMessage.js
+++ 06-chat/client-chat/src/components/SendMessage.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SendMessage = () => {
+
+
+    const [message, setMessage] = useState('')
+
+    const onChange = ({target}) =>{
+        setMessage(target.value)
+    }
+    const onSubmit = (ev) =>{
+        ev.preventDefault()
+
+        if(message.length === 0) {return}
+
+        console.log(message)
+        setMessage('')
+
+        //TODO: Emitir al backend sockets para enviar mensaje
+        
+    }
+
+
     return (
-        <form>
+        <form onSubmit={onSubmit}>
             <div className="type_msg row">
                 <div className="input_msg_write col-sm-9">
-                    <input type="text" className="write_msg" placeholder="Mensaje..." />
+                    <input type="text" className="write_msg" placeholder="Mensaje..." value={message} onChange={onChange}/>
                 </div>
                 <div className="col-sm-3 text-center">
                     <button className="msg_send_btn mt-3" type="submit">
